1d789f7dd9546a6cce06bed49d1e5fe0d7ded004
Sara Mohammadi <sara.mohammadi@emotion.com.ua>
2022-08-25 15:13:43 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Layout/Header";
+import Info from "./components/PersonalInfo/Info";
+import Bio from "./components/PersonalInfo/Bio";
+import Cv from "./components/PersonalInfo/Cv";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header />
+      <main className="main">
+        <Info />
+        <Bio />
+        <Cv />
+      </main>
+    </Fragment>
   );
 }
 
